ff48d18998625f36f6d6371c1e485fb848d19f95
Aria Hogan <hoganc3@wit.edu>
2021-11-20 14:59:38 -0500

    Wah

--- script.py
+++ script.py
@@ -1,5 +1,25 @@
 import urllib.request
 
+
+def createLists(lines):
+    for str in lines:
+        if '["price"]' in str:
+            price.append(str)
+        elif '["name"]' in str:
+            names.append(str)
+        else:
+            pass
+
+
+def fixString(arr):
+    for i, str in enumerate(arr):
+        s = (f'[{i}]')
+        words = ['["price"]', '["name"]', '"', ';', 'gtm_categories','\r','=', s,' ']
+        for word in words:
+            str = str.replace(word, '')
+        return str
+
+
 url = 'https://www.liveaquaria.com/divers-den/category/3/marine-fish'
 r = urllib.request.urlopen(url)
 
@@ -10,17 +30,7 @@ names = []
 price = []
 
 
-def createLists(lines):
-    for x in lines:
-        if '["price"]' in x:
-            price.append(x.replace('gtm_categories', '').replace('["price"]',''))
-        elif '["name"]' in x:
-            names.append(x)
-        else:
-            pass
-
-
 createLists(lines)
-
+fixString(price)
 for x in price:
-    print(f"{x}\n")
+    print(f"{x}")
