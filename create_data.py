5b0d29013549197a7f8adcb1a59cb2e56c605117
Aria Hogan <hoganc3@wit.edu>
2021-11-21 13:10:23 -0500

    Finished

---
+++ create_data.py
@@ -0,0 +1,59 @@
+'''Functions For Getting And Creating Usable Data'''
+
+import urllib.request
+
+def get_data(page_url):
+    '''get_data fetches data from a site
+
+    :URL: The url to scrape
+    :return: list of page source seperated by line
+    '''
+    with urllib.request.urlopen(page_url) as file:
+        text = file.read()
+    return text.decode("utf-8").split('\n')
+
+
+def create_lists(lines, mask_string, my_list=None):
+    '''create_lists creates a list for the wanted value, ex: name, price
+
+    :lines: list of source code by line
+    :str: used to mask data from source code
+    :list=None: initialize list
+    :return: list of wanted values
+    '''
+    if my_list is None:
+        my_list = []
+    mask = (f'["{mask_string}"]')
+    for string in lines:
+        if mask in string:
+            my_list.append(string)
+        else:
+            pass
+    fix_string(my_list)
+    return my_list
+
+
+def fix_string(my_list):
+    '''fix_string removed unwanted characters from string
+
+    :list: list to be fixed
+    :return: list with fixed strings
+    '''
+    for i, string in enumerate(my_list):
+        mask = (f'[{i}]')
+        words = ['["price"]', '["name"]', '"',
+                 ';', 'gtm_categories', '\r', '=',"\\", mask]
+        for word in words:
+            if word in string:
+                my_list[i] = my_list[i].replace(word, '').strip()
+    return my_list
+
+def arr_to_int(my_list):
+    '''arr_to_int makes all elements in list a float
+
+    :my_list: list to fix
+    :return: fixed list
+    '''
+    for i, string in enumerate(my_list):
+        my_list[i] = float(string)
+    return my_list
--- main.py
+++ main.py
@@ -1,35 +1,83 @@
 '''Runs program'''
-
-import urllib.request
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import create_list as cl
+import create_data as cd
 
-URL = 'https://www.liveaquaria.com/divers-den/category/3/marine-fish'
-with urllib.request.urlopen(URL) as r:
-    text = r.read()
 
-lines = text.decode("utf-8").split('\n')
-names = cl.create_arrays(lines, "name")
-price = cl.create_arrays(lines, "price")
-price = cl.arr_to_int(price)
+def prompt():
+    '''Prompts the user what information they want to see.
 
-for i, x in enumerate(price):
-    print(names[i])
-    print(price[i])
+    :returns: page URL
+    '''
+    page_list = {
+        "Marine Fish": "https://www.liveaquaria.com/divers-den/category/3/marine-fish",
+        "Freshwater": "https://www.liveaquaria.com/divers-den/category/11/freshwater",
+        "Betta Fish": "https://www.liveaquaria.com/divers-den/category/12/betta-fish",
+        "Inverts": "https://www.liveaquaria.com/divers-den/category/4/inverts",
+        "Clams": "https://www.liveaquaria.com/divers-den/category/1/clams",
+        "Maricultured Corals":
+            "https://www.liveaquaria.com/divers-den/category/6/maricultured-corals",
+        "Aquacultured Corals":
+            "https://www.liveaquaria.com/divers-den/category/5/aquacultured-corals",
+        "Soft Corals":
+            "https://www.liveaquaria.com/divers-den/category/7/polyp-mushroom-and-soft-corals",
+        "LPS Corals": "https://www.liveaquaria.com/divers-den/category/8/lps-corals",
+        "SPS Corals": "https://www.liveaquaria.com/divers-den/category/2/sps-corals",
+        "NPS Corals": "https://www.liveaquaria.com/divers-den/category/10/non-photosynthetic-nps",
+        "Deals": "https://www.liveaquaria.com/divers-den/category/9/deals-steals"
+    }
+    print("""Welcome to the LiveAquaria Scraper!
+    What information would you like to know about?
+    Please pick one from the following
+    \t- Marine Fish
+    \t- Freshwater
+    \t- Betta Fish
+    \t- Inverts
+    \t- Clams
+    \t- Maricultured Corals
+    \t- Aquacultured Corals
+    \t- Soft Corals
+    \t- LPS Corals
+    \t- SPS Corals
+    \t- NPS Corals
+    \t- Deals
+    """)
 
-data = {
-        'Fish Name': names,
+    not_answered = True
+    while not_answered:
+        answer = input("> ")
+        if answer in page_list:
+            return page_list[answer]
+
+
+def display_data():
+    '''Creates and displays data graph
+
+    :names: List of item name
+    :price: Price of item
+    '''
+    data = {
+        'Name': names,
         'Price': price
-        }
+    }
 
-df = pd.DataFrame(data, columns=['Fish Name', 'Price'])
-df = df.sort_values(by="Price")
-df["Number"] = list(range(0, len(names)))
-df = df.set_index('Number')
-print(df)
-df.plot(kind='scatter',x="Fish Name",y="Price");
-plt.xticks(rotation=90)
-plt.tight_layout()
-plt.show()
+    data_frame = pd.DataFrame(data, columns=['Name', 'Price'])
+    data_frame = data_frame.sort_values(by="Price")
+    data_frame["Number"] = list(range(0, len(names)))
+    data_frame = data_frame.set_index('Number')
+    print(data_frame.to_string())
+
+    data_frame.plot(kind='scatter', x="Name", y="Price")
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.subplots_adjust(top=.98, left=.05, right=.98)
+    plt.show()
+
+
+URL = prompt()
+lines = cd.get_data(URL)
+
+names = cd.create_lists(lines, "name")
+price = cd.create_lists(lines, "price")
+price = cd.arr_to_int(price)
+display_data()
